12452cc2bf5fc8c85ecb8c94045d0cf2c5f068e3
Niklas Reimer <n.reimer@uni-luebeck.de>
2024-02-21 09:21:12 +0100

    extend settings for mapping

---
+++ src/main/java/de/uksh/medic/etl/settings/Mapping.java
@@ -0,0 +1,45 @@
+package de.uksh.medic.etl.settings;
+
+/**
+ * Defines mappings between source and target profiles used for MDR mapping.
+ */
+public class Mapping {
+
+    private String source;
+    private String sourceVersion;
+    private String target;
+    private String targetVersion;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getSourceVersion() {
+        return sourceVersion;
+    }
+
+    public void setSourceVersion(String sourceVersion) {
+        this.sourceVersion = sourceVersion;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public String getTargetVersion() {
+        return targetVersion;
+    }
+
+    public void setTargetVersion(String targetVersion) {
+        this.targetVersion = targetVersion;
+    }
+
+}
--- src/main/java/de/uksh/medic/etl/settings/Settings.java
+++ src/main/java/de/uksh/medic/etl/settings/Settings.java
@@ -3,6 +3,7 @@ package de.uksh.medic.etl.settings;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.net.URL;
+import java.util.Map;
 
 /**
  * Settings for OpenEhrObds.
@@ -12,6 +13,8 @@ public final class Settings {
 
     private static URL fhirTsUrl;
     private static CxxMdrSettings cxxmdr;
+    private static Map<String, Mapping> mapping;
+    private static int depthLimit;
 
     private Settings() {}
 
@@ -31,6 +34,24 @@ public final class Settings {
     @JsonProperty("cxxmdr")
     public void setCxxMdrSettings(CxxMdrSettings newCxxMdr) {
         cxxmdr = newCxxMdr;
+    }
+
+    public static Map<String, Mapping> getMapping() {
+        return mapping;
+    }
+
+    @JsonProperty("mapping")
+    public void setMapping(Map<String, Mapping> newMapping) {
+        mapping = newMapping;
+    }
+
+    public static int getDepthLimit() {
+        return depthLimit;
+    }
+
+    @JsonProperty("depthLimit")
+    public void setDepthLimit(int newDepthLimit) {
+        depthLimit = newDepthLimit;
     }
 
 }
