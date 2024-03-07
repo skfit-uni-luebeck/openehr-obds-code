3315a5ea29e6eddf615525459451c100766c2b08
Niklas Reimer <n.reimer@uni-luebeck.de>
2024-03-07 17:36:09 +0100

    use non static generator

--- src/main/java/de/uksh/medic/etl/openehrmapper/EHRParser.java
+++ src/main/java/de/uksh/medic/etl/openehrmapper/EHRParser.java
@@ -17,7 +17,6 @@ import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Map;
 import javax.xml.bind.JAXBException;
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
@@ -30,7 +29,30 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class EHRParser {
-    public Composition build(String xml, Map<String, Object> map)
+
+    private Document doc;
+    private Generator g;
+
+    public EHRParser(String xml) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(false); // never forget this!
+        try {
+            doc = factory.newDocumentBuilder().parse(new InputSource(new StringReader(xml)));
+            g = new Generator(doc);
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SAXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public Composition build(Map<String, Object> map)
             throws ParserConfigurationException, SAXException, XPathExpressionException, IOException,
             JAXBException {
 
@@ -38,17 +60,11 @@ public class EHRParser {
                 + "/attributes[rm_attribute_name=\"content\"]";
         String pathContext = "/template/definition[rm_type_name = \"COMPOSITION\"]"
                 + "/attributes[rm_attribute_name=\"context\"]/children/attributes[rm_attribute_name=\"other_context\"]";
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
         XPathFactory xpf = XPathFactory.newInstance();
         XPath xp = xpf.newXPath();
 
         Composition composition = new Composition();
-
-        factory.setNamespaceAware(false); // never forget this!
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        InputSource is = new InputSource(new StringReader(xml));
-        Document doc = builder.parse(is);
 
         composition.setArchetypeNodeId(
                 ((String) xp.evaluate("/template/definition/archetype_id", doc, XPathConstants.STRING))
@@ -78,19 +94,17 @@ public class EHRParser {
         composition.setComposer(new PartySelf());
         map.put("start_time", map.getOrDefault("start_time", LocalDateTime.now().toString()));
 
-        Generator g = new Generator(doc);
-        Map<String, Object> applyMap = Generator.applyDefaults(map);
-
+        Map<String, Object> applyMap = g.applyDefaults(map);
         ArrayList<ContentItem> content = new ArrayList<ContentItem>();
         composition.setContent(content);
-        Generator.processAttributeChildren(pathContent, composition.getArchetypeNodeId(), content, applyMap);
+        g.processAttributeChildren(pathContent, composition.getArchetypeNodeId(), content, applyMap);
 
         EventContext context = new EventContext(new DvDateTime((String) map.get("start_time")),
                 new DvCodedText("other care", new CodePhrase(new TerminologyId("openehr"), "238")));
         composition.setContext(context);
         ItemTree itemTree = new ItemTree();
         context.setOtherContext(itemTree);
-        Generator.processAttributeChildren(pathContext, composition.getArchetypeNodeId(), itemTree, applyMap);
+        g.processAttributeChildren(pathContext, composition.getArchetypeNodeId(), itemTree, applyMap);
 
         return composition;
 
